ca38e96c198bc60a5eaf92fc2437eda462679504
김대희 <0824dh@naver.com>
2023-05-17 10:50:15 +0900

    Initial Commit

---
+++ Greeny-iOS/Greeny_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greeny_iOSApp.swift
+//  Greeny-iOS
+//
+//  Created by 김대희 on 2023/05/17.
+//
+
+import SwiftUI
+
+@main
+struct Greeny_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
